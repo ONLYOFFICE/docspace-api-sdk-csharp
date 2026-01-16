467c7fa2fd2463305dff450600c48321f5b2ba7f
diana-vahomskaya <diana.vahomskaya@onlyoffice.com>
2026-01-16 19:24:40 +0300

    fix

--- src/DocSpace.API.SDK/Model/WhiteLabelLogoType.cs
+++ src/DocSpace.API.SDK/Model/WhiteLabelLogoType.cs
@@ -24,84 +24,84 @@ namespace DocSpace.API.SDK.Model
     public enum WhiteLabelLogoType
     {
         /// <summary>
-        /// Enum NUMBER_1 for value: 1
+        /// Enum LightSmall for value: 1
         /// </summary>
-        NUMBER_1 = 1,
+        LightSmall = 1,
 
         /// <summary>
-        /// Enum NUMBER_2 for value: 2
+        /// Enum LoginPage for value: 2
         /// </summary>
-        NUMBER_2 = 2,
+        LoginPage = 2,
 
         /// <summary>
-        /// Enum NUMBER_3 for value: 3
+        /// Enum Favicon for value: 3
         /// </summary>
-        NUMBER_3 = 3,
+        Favicon = 3,
 
         /// <summary>
-        /// Enum NUMBER_4 for value: 4
+        /// Enum DocsEditor for value: 4
         /// </summary>
-        NUMBER_4 = 4,
+        DocsEditor = 4,
 
         /// <summary>
-        /// Enum NUMBER_5 for value: 5
+        /// Enum DocsEditorEmbed for value: 5
         /// </summary>
-        NUMBER_5 = 5,
+        DocsEditorEmbed = 5,
 
         /// <summary>
-        /// Enum NUMBER_6 for value: 6
+        /// Enum LeftMenu for value: 6
         /// </summary>
-        NUMBER_6 = 6,
+        LeftMenu = 6,
 
         /// <summary>
-        /// Enum NUMBER_7 for value: 7
+        /// Enum AboutPage for value: 7
         /// </summary>
-        NUMBER_7 = 7,
+        AboutPage = 7,
 
         /// <summary>
-        /// Enum NUMBER_8 for value: 8
+        /// Enum Notification for value: 8
         /// </summary>
-        NUMBER_8 = 8,
+        Notification = 8,
 
         /// <summary>
-        /// Enum NUMBER_9 for value: 9
+        /// Enum SpreadsheetEditor for value: 9
         /// </summary>
-        NUMBER_9 = 9,
+        SpreadsheetEditor = 9,
 
         /// <summary>
-        /// Enum NUMBER_10 for value: 10
+        /// Enum SpreadsheetEditorEmbed for value: 10
         /// </summary>
-        NUMBER_10 = 10,
+        SpreadsheetEditorEmbed = 10,
 
         /// <summary>
-        /// Enum NUMBER_11 for value: 11
+        /// Enum PresentationEditor for value: 11
         /// </summary>
-        NUMBER_11 = 11,
+        PresentationEditor = 11,
 
         /// <summary>
-        /// Enum NUMBER_12 for value: 12
+        /// Enum PresentationEditorEmbed for value: 12
         /// </summary>
-        NUMBER_12 = 12,
+        PresentationEditorEmbed = 12,
 
         /// <summary>
-        /// Enum NUMBER_13 for value: 13
+        /// Enum PdfEditor for value: 13
         /// </summary>
-        NUMBER_13 = 13,
+        PdfEditor = 13,
 
         /// <summary>
-        /// Enum NUMBER_14 for value: 14
+        /// Enum PdfEditorEmbed for value: 14
         /// </summary>
-        NUMBER_14 = 14,
+        PdfEditorEmbed = 14,
 
         /// <summary>
-        /// Enum NUMBER_15 for value: 15
+        /// Enum DiagramEditor for value: 15
         /// </summary>
-        NUMBER_15 = 15,
+        DiagramEditor = 15,
 
         /// <summary>
-        /// Enum NUMBER_16 for value: 16
+        /// Enum DiagramEditorEmbed for value: 16
         /// </summary>
-        NUMBER_16 = 16
+        DiagramEditorEmbed = 16
     }
 
 }
